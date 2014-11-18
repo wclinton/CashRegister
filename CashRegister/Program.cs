f95c3517efdad4f570d434b9a9f3791c66680d75
Wade Clinton <wade.clinton@sage.com>
2014-11-18 11:22:28 -0800

    Add loaddata feature

--- CashRegister/Program.cs
+++ CashRegister/Program.cs
@@ -9,31 +9,56 @@ namespace CashRegister
 {
     class Program
     {
-        private static void Main()
-        {                   
-            LoadData();
+        private static bool loadData;
+
+        private static void Main(string [] args)
+        {
+
+            ParseArgs(args);
+
+            if (loadData)
+            { 
+                LoadData();
+            }
+
+            Console.WriteLine("Loading the current order...");          
             var order = ReadFromJson<Order>("orders.json");
+            Console.WriteLine("Processing the order...");          
             var cr = new CashRegister(new CashRegisterContext());
 
             var total =cr.PriceOrder(order);
 
-            Console.WriteLine("The total for the order is: {0}",total.ToString("C"));
-            Console.ReadLine();          
+            Console.WriteLine("The total for the order is: {0}",total.ToString("C"));            
+        }
+
+        private static void ParseArgs(IEnumerable<string> args)
+        {
+            foreach (var arg in args)
+            {
+                if (String.Compare("-loaddata", arg.Trim(), StringComparison.OrdinalIgnoreCase) == 0)
+                    loadData = true;
+            }
         }
 
         private static void LoadData()
         {
             using (var db = new CashRegisterContext())
-            {                
+            {
+
+                Console.WriteLine("Clearing the Database...");            
                 //Clear the database 
                 db.Items.RemoveRange(db.Items);
                 db.ItemPrices.RemoveRange(db.ItemPrices);
                 db.Coupons.RemoveRange(db.Coupons);
                 db.ItemDiscounts.RemoveRange(db.ItemDiscounts);
-               
+
+                Console.WriteLine("Loading Items...");    
                 db.Items.AddRange(GetSampleItems());
+                Console.WriteLine("Loading ItemPrices...");    
                 db.ItemPrices.AddRange(GetSampleItemPrices());
+                Console.WriteLine("Loading Coupons...");    
                 db.Coupons.AddRange(GetSampleCoupons());
+                Console.WriteLine("Loading Disounts...");    
                 db.ItemDiscounts.AddRange(GetItemDiscounts());
                 db.SaveChanges();
             }         
@@ -77,7 +102,7 @@ namespace CashRegister
 
         static T ReadFromJson<T>(string filename)
         {
-            using (var r = new StreamReader(filename))
+            using (var r = new StreamReader("Json\\"+filename))
             {
                 var json = r.ReadToEnd();
                 var items = JsonConvert.DeserializeObject<T>(json);
